035dabb5026c17f7d04babd502628a08f0994c37
Shomari <nyusternie@sdot.io>
2025-11-26 17:47:39 -0500

    Upgrade test pkgs.

--- packages/Platform/vitest.config.ts
+++ packages/Platform/vitest.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'node',  // For fetch() API calls
-    pool: 'threads',  // Fixes CJS warning
     globals: true,
     setupFiles: ['./tests/setup.ts']
   }
